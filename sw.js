09709019d90e1e1202bc0643516947e01f7f2571
recesar10 <recesar@gmail.com>
2026-09-09 08:23:29 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE='diario-pressao-familia-araujo-v5';
+const ASSETS=['./','./index.html','./manifest.json','./logo.jpg','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).catch(()=>{}));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',event=>{
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      return cached || fetch(event.request).then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+        return response;
+      }).catch(()=>cached);
+    })
+  );
+});
